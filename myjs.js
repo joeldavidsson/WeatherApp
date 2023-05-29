f68f8721b87ad1a38a49acf94e3db45983e1a1f8
joeldavidsson <127835196+joeldavidsson@users.noreply.github.com>
2023-05-29 19:53:27 +0200

    Uppdaterat css, js och html
    HTML: lagt till wrappers, id och innehåll i footer.
    CSS: styling och responsivitet
    JS:  lagt till fler datavärden hämtat från API.
    Fixat så att icon visas beroende vilket väder det är. Kopplat id från API till ikon-url.
    Lagt till funktion för tid och datum.
    Lagt till funktion för att få stor bokstav i början av ord.
    Lagt till intervaller för funktionerna.
    
    Lite småfix kvar.

--- myjs.js
+++ myjs.js
@@ -1,51 +1,90 @@
 let body = document.body;
 let cityName = document.getElementById("cityName");
 let weatherInfo = document.getElementById("weatherInfo");
-let weatherIcon = document.getElementById("weatherIcon");
+let weatherIcon = document.getElementById("weatherIcon")
 
 
+// fetching weather data from API
 fetch("https://api.openweathermap.org/data/2.5/weather?lat=56.67&lon=12.85&appid=fa2504dd66590b3d35b34eee87af1956&units=metric")
   .then((response) => response.json())
-  .then(weatherData => hVader(weatherData));
-  
-  
+  .then(weatherData => halmstadWeather(weatherData));
 
-function hVader(weatherData) {
+
+//function to get weather data for Halmstad, creating elements and showing the data in HTML.
+function halmstadWeather(weatherData) {
+  
   let name = weatherData.name;
   let coordLon = weatherData.coord.lon;
   let coordLat = weatherData.coord.lat;
   let temperature = weatherData.main.temp;
+  let feelsLikeTemp = weatherData.main.feels_like;
   let pressure = weatherData.main.pressure;
-  
+  let humidity = weatherData.main.humidity;
   let wDescription = weatherData.weather[0].description;
+  let wind = weatherData.wind.speed;
   let wIcon = weatherData.weather[0].icon;
+
 
   let city = document.createElement("h1");
   city.innerText = name;
-  
+
   let coordItem = document.createElement("h4");
-  coordItem.innerHTML = "Longitude " + coordLon + "<br>Latitude: " + coordLat;
+  coordItem.innerHTML = "Longitude " + coordLon + " Latitude: " + coordLat;
 
-  let temperatureItem = document.createElement("div");
-  temperatureItem.innerText = "Temperature: " + temperature + "°C";
+  let temperatureItem = document.createElement("li");
+  temperatureItem.innerText = "Temperature: " + temperature + " °C";
 
-  let pressureItem = document.createElement("div");
+  let feelsLikeItem = document.createElement("li");
+  feelsLikeItem.innerText = "Feels like: " + feelsLikeTemp + " °C";
+
+  let pressureItem = document.createElement("li");
   pressureItem.innerText = "Pressure: " + pressure + " hPa";
 
-  let descriptionItem = document.createElement("div");
-  descriptionItem.innerText = "Description: " + wDescription;
+  let humidityItem = document.createElement("li");
+  humidityItem.innerText = "Humidity: " + humidity + " %";
+
+  let descriptionItem = document.createElement("li");
+  descriptionItem.innerText = "Description: " + capLetter(wDescription);
+
+  let windItem = document.createElement("li");
+  windItem.innerText = "Wind: " + wind + " m/s";
 
   let iconItem = document.createElement("img");
-  iconItem.innerHTML = "Icon: " + wIcon;
-  iconItem.classList = "wIcon";
-
+  iconItem.src = "https://openweathermap.org/img/wn/" + wIcon + "@4x.png";
+  iconItem.innerHTML = iconItem;
+  iconItem.classList = "weatherIcon";
 
   cityName.append(city, coordItem);
-  weatherInfo.append(temperatureItem, descriptionItem, pressureItem)
-  weatherIcon.append(iconItem);
-
-  
-  console.log(weatherData)
-  
+  weatherInfo.append(temperatureItem, feelsLikeItem, descriptionItem, pressureItem, humidityItem, windItem)
+  weatherIcon.append(iconItem)
 }
 
+// function to make first letter uppercase in string
+function capLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// function to show current weekday, date and time
+function currentTimeDate() {
+  let currentTime = new Date();
+
+  let weekdayArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let weekday = weekdayArr[currentTime.getDay()];
+
+  let hour = currentTime.getHours();
+  let min = currentTime.getMinutes();
+  let sec = currentTime.getSeconds();
+  let year = currentTime.getFullYear();
+  let day = currentTime.getDate();
+  let month = currentTime.getMonth() + 1;
+  document.getElementById("dateTime").innerHTML = weekday + "<br>" + hour + ":" + min + ":" + sec + "<br>" + year + "/" + month + "/" + day;
+}
+
+
+
+// set intervals for calling functions:
+// call currentTimeDate every second
+setInterval(currentTimeDate, 1000);
+
+//call halmstadWeather every 15 mins
+setInterval(halmstadWeather, 900000);
